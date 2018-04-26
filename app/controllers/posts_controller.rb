c4345e2425286d4172242dca44f71263ca9aed4f
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-04-26 16:27:18 -0700

    sdksf

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,7 @@ class PostsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
     # prevents csrf blocking
-    # protect_from_forgery with: :null_session
-    skip_before_action :verify_authenticity_token
+    protect_from_forgery with: :null_session
 
     def index 
         @posts = Post.all
