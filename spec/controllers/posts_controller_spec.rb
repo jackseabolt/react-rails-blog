2a2866868a8ed8ae8acdbd2edd5745974cab906a
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-04-26 21:07:51 -0700

    rspec updated

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,16 +2,25 @@ require 'rails_helper'
 
 
 describe PostsController do 
-    it 'should return success response' do 
-        get :index 
-        expect(response).to have_http_status(200)
-    end 
+    describe 'index' do 
 
-    it 'should return an array' do 
-        FactoryGirl.create_list :post, 2
-        get :index
-        json = JSON.parse(response.body)
-        pp json
-        expect(json).to be_an_instance_of(Array)
+        before(:all) do
+            FactoryGirl.create_list :post, 2
+        end
+
+        it 'should return success response' do 
+            get :index 
+            expect(response).to have_http_status(200)
+        end 
+
+        it 'should return an array of all posts' do 
+            get :index
+            body = JSON.parse(response.body)
+            pp body
+            expect(body).to be_an_instance_of(Array)
+            expect(body.length).to eq(2)
+            expect(body[0]["title"]).to eq("MyTitle")
+            expect(body[0]["content"]).to eq("MyContent")
+        end 
     end 
 end 
